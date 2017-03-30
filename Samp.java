e3cbb52150b332ab80a893abc83fff81c7271b6a
bhahu <bhahu.s.n@gmail.com>
2017-03-30 13:43:27 +0530

    Add files via upload

---
+++ Samp.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Samp 
+{
+
+public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    String username1; // Username of person
+    String password1; // Password of person
+    System.out.println (" Please enter a username:()");
+    username1 = "";
+    System.out.println (" Please enter a password1");
+    password1="";
+    if (username1== "Brian")
+    System.out.println("Welcome");
+    else
+    System.out.println ("Incorrect username");
+    if (password1 == "spam")
+    System.out.println ("Welcome");
+    else
+    System.out.println("Incorrect password");
+}}
